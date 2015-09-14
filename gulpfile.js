8006938c1779abe0b1c79b40323767d950c6eb39
rlemon <rob.lemon@gmail.com>
2015-09-14 11:30:33 -0400

    fixed polyfills

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp'),
 	babel = require('gulp-babel'),
-	concat = require('gulp-concat');
-	
-// because I don't know browserfy.. 
-var polyfill = 'node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.min.js';
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
 
-gulp.task('es6to5', function() {
-	return gulp.src([polyfill, 'src/js/watcher.babel.js', 'src/js/parsers/*.babel.js'])
+gulp.task('default', function() {
+	return gulp.src(['src/polyfills/*', 'src/js/watcher.babel.js', 'src/js/parsers/*.babel.js'])
 			.pipe(concat('script.js'))
 			.pipe(babel())
+			.pipe(uglify())
 			.pipe(gulp.dest('dist'));
 });

407e2ea7a4beb74f537c64d5499f73b7ce43b7a4
rlemon <rob.lemon@gmail.com>
2015-09-28 17:22:54 -0400

    returning some indenting

--- src/js/plugins/monoInput.js
+++ src/js/plugins/monoInput.js
@@ -3,6 +3,18 @@
     let input = document.getElementById('input');
     let monoFont = 'Hack, monospace';
     let defaultFont = window.getComputedStyle(input).fontFamily;
+    input.addEventListener('keydown', e => {
+        if (e.shiftKey && e.keyCode === 13) {
+            e.preventDefault();
+            let lastLine = input.value.split(/\r\n?|\n/).pop();
+            input.value += '\r\n';
+            let lastLineSpaces = lastLine.match(/^\s+/);
+            lastLineSpaces = lastLineSpaces ? lastLineSpaces[0] : '';
+            if (lastLineSpaces.length >= 4) {
+                input.value += '    ';
+            }
+        }
+    });
     input.addEventListener('keyup', e => {
         let lines = input.value.split(/\r\n?|\n/);
         let isCode = lines.every(line => line.substr(0, 4) === '    ');

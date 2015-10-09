610d439bfcaa81e004caaf4ed7ae4b97bd8e48d3
rlemon <rob.lemon@gmail.com>
2015-10-09 16:23:01 -0400

    fixed a bunch of bugs

--- src/js/parsers/webmOnebox.js
+++ src/js/parsers/webmOnebox.js
@@ -4,6 +4,7 @@ const webmOneboxParser = node => {
 		if( content.childNodes.length > 1 ) return; // https://github.com/rlemon/se-chat-dark-theme/issues/30
 		let anchor = content.querySelector('a');
 		if( !anchor || !/(webm|gifv)$/.test(anchor.href) ) return; // failed to get anchor, or anchor isn't a webm/gifv
+		if( anchor.rel === 'nofollow' ) return; // user changed the anchor text. [hello world](webm/gifv link) adds a nofollow to the link. this is the only way I could determine this.
 		let vid = document.createElement('video');
 		vid.controls = true;
 		vid.src = anchor.href.replace(/(gifv)$/,'webm');

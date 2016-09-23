71ecc824ce0ca60d4ff02e864316ee486c518a5d
rlemon <rob.lemon@gmail.com>
2016-09-23 15:39:31 -0400

    fixed documentation css and mp4 videos, changed minor css

--- src/js/parsers/webmOnebox.js
+++ src/js/parsers/webmOnebox.js
@@ -3,11 +3,11 @@ const webmOneboxParser = node => {
 		let content = node.querySelector('.content');
 		if( content.childNodes.length > 1 ) return; // https://github.com/rlemon/se-chat-dark-theme/issues/30
 		let anchor = content.querySelector('a');
-		if( !anchor || !/(webm|gifv)$/.test(anchor.href) ) return; // failed to get anchor, or anchor isn't a webm/gifv
+		if( !anchor || !/(webm|gifv|mp4)$/.test(anchor.href) ) return; // failed to get anchor, or anchor isn't a webm/gifv
 		if( anchor.rel === 'nofollow' ) return; // user changed the anchor text. [hello world](webm/gifv link) adds a nofollow to the link. this is the only way I could determine this.
 		let vid = document.createElement('video');
 		vid.controls = true;
-		vid.src = anchor.href.replace(/(gifv)$/,'webm');
+		vid.src = anchor.href.replace(/(gifv|webm)$/,'mp4');
 		vid.width = 320;
 		vid.height = 240;
 		content.replaceChild(vid, anchor);

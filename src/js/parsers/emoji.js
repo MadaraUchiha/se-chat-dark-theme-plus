d4454670319eaaea9689cbe2dfa7db0eeca8de17
rlemon <rob.lemon@gmail.com>
2016-11-04 11:20:43 -0400

    emoji regexp was flawed, probably still is

--- src/js/parsers/emoji.js
+++ src/js/parsers/emoji.js
@@ -1,7 +1,7 @@
 function emojiReplacer(node) {
   if (!node.classList || !node.classList.contains('message') || node.classList.contains('pending')) return;
   parseTextNodes(node, textNode => {
-    textNode.textContent = textNode.textContent.replace(/\B:\w+:\B/g, match => {
+    textNode.textContent = textNode.textContent.replace(/\B:[^\s|:]+:\B/g, match => {
 
       const found = emojiData.find(item => {
         if (item.triggers.includes(match.slice(1, -1))) {

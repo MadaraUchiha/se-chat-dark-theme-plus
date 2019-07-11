ae88869501a8a883f36ccdc567fb6ad33b19e832
Madara Uchiha <me@madara.ninja>
2019-07-11 21:31:05 +0300

    Implement support for uploading images through paste.

--- src/modules/BetterImageUploadsModule.ts
+++ src/modules/BetterImageUploadsModule.ts
@@ -1,17 +1,103 @@
+const IMAGE_TYPE = /image.*/;
+
+declare global {
+  function fkey(): { fkey: string };
+  interface Window {
+    fkey(): { fkey: string };
+    eval(js: string): unknown;
+  }
+}
+
 export class BetterImageUploadsModule {
-  private inputElement = document.querySelector<HTMLTextAreaElement>('#input');
+  private inputElement = document.querySelector<HTMLTextAreaElement>("#input");
+  private container = document.querySelector<HTMLDivElement>("#bubble");
+  private loading = document.createElement("span");
 
   public init() {
-    window.addEventListener('paste', this.handlePaste);
-    window.addEventListener('dragover', event => event.target !== this.inputElement && event.preventDefault());
-    window.addEventListener('drop', this.handleDrop);
+    window.addEventListener("paste", this.handlePaste.bind(this));
+    window.addEventListener(
+      "dragover",
+      event => event.target !== this.inputElement && event.preventDefault()
+    );
+    window.addEventListener("drop", this.handleDrop.bind(this));
+    if (this.container) {
+      this.container.appendChild(this.loading);
+      this.loading.classList.add("dark-theme-loading");
+    }
   }
 
-  private handlePaste(e: Event) {
+  private async handlePaste(e: Event) {
     const event = e as ClipboardEvent;
-    const item = event.clipboardData!.items[0];
-    if (event.target === this.inputElement && item.type === 'text/plain') { return; }
-    console.info({ item });
+    const items = [...event.clipboardData!.items];
+    const types = [...event.clipboardData!.types];
+
+    const item = items.find(
+      (item, i) => item.type.match(IMAGE_TYPE) || types[i].match(IMAGE_TYPE)
+    );
+
+    if (!item) {
+      return;
+    }
+
+    const file = item.getAsFile();
+
+    if (!file) {
+      return;
+    }
+
+    const url = await this.uploadFile(file);
+    this.appendToInput(url);
+  }
+
+  private appendToInput(substring: string) {
+    if (!this.inputElement) {
+      return;
+    }
+    this.inputElement.value += substring;
+  }
+
+  private async uploadFile(file: File) {
+    // This lovely piece of work is due to how Firefox handles global variables.
+    // A content script cannot normally access global variables, however, eval works.
+    // eval() will be executed in the context of the script, and window.eval(), in the context of the page.
+    // The following will access the global fkey() function on the page.
+    const fkey = window.eval("fkey().fkey") as string;
+
+    const form = new FormData();
+
+    form.append("filename", file);
+    form.append("fkey", fkey);
+
+    this.setLoadingState(true);
+    try {
+      const resp = await fetch("https://chat.stackoverflow.com/upload/image", {
+        method: "post",
+        body: form
+      });
+
+      const html = await resp.text();
+
+      return this.parseResponse(html);
+    } finally {
+      this.setLoadingState(false);
+    }
+  }
+
+  private parseResponse(htmlResponse: string) {
+    const match = htmlResponse.match(/var result = '(.+)'/);
+
+    if (!match) {
+      console.error("Failed to parse the response. Tell Madara he's an idiot.");
+      throw new Error(
+        "Failed to parse the response. Tell Madara he's an idiot"
+      );
+    }
+
+    return match[1];
+  }
+
+  private setLoadingState(isLoading: boolean) {
+    this.loading.textContent = isLoading ? "Loading..." : "";
   }
 
   private handleDrop(e: DragEvent) {
